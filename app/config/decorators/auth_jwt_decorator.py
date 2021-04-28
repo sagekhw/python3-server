88140b9327b05e6894f278778bab1b2edf774ae9
sagekhw <sagekhw@gmail.com>
2021-04-28 17:14:26 +0900

    clean code

--- app/config/decorators/auth_jwt_decorator.py
+++ app/config/decorators/auth_jwt_decorator.py
@@ -1,24 +1,3 @@
-#   def login_required(f):
-#       @wraps(f)
-#       def decorated_function(*args, **kwargs):
-#           access_token = request.headers.get("Authorization")
-#           if access_token is not None:
-#               try:
-#                   payload = jwt.decode(access_token, app.config["JWT_SECRET_KEY"], "HS256")
-#               except jwt.InvalidTokenError:
-#                   payload = None
-	
-#               if payload is None:
-#                   return Response(status=401)
-	
-#               email = payload["email"]
-#               g.email = email
-#               g.email = get_user_info(email) if email else None
-#           else:
-#               return Response(status=401)
-	
-#           return f(*args, **kwargs)
-#       return decorated_function
 from functools import wraps
 from flask_jwt_extended import ( verify_jwt_in_request, jwt_required,  get_jwt_identity, get_jwt)
 
