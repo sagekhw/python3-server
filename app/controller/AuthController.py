4c1d962326c2779436bb74833309debd4eff0904
sagekhw <sagekhw@gmail.com>
2021-04-28 17:01:32 +0900

    add login,register bcrypt

--- app/controller/AuthController.py
+++ app/controller/AuthController.py
@@ -3,14 +3,11 @@ from flask_jwt_extended import (JWTManager, jwt_required, create_access_token, g
 from app.config.decorators.auth_jwt_decorator import *
 from app.entities.user import *
 from app.services.userService import *
+import bcrypt
 
 auth = Blueprint('AuthController', __name__, url_prefix='/auth')
 
 user_service = userService()
-
-#################################################
-# 로그인 API 영역
-
 
 @auth.route("/register", methods=["POST"])
 def register():
@@ -19,17 +16,9 @@ def register():
 	user_password = input_data['password']
 	user_role = input_data['role']
 
-	if(user_role == 'role_user' or user_role == 'role_admin'):
-		userObj = user(email=user_email,password=user_password,role=user_role)
-		result = user_service.insert_by_register(userObj)
-		if result:
-			return "success"
-		else:
-			return "false"
-	else:
-		return jsonify(
-			result = "Invalid Params!"
-			)
+	input_user = user(user_email,user_password,user_role)
+	print("input data : ",input_user.email)
+	return user_service.user_register(input_user)
 
 
 @auth.route('/login', methods=['POST'])
@@ -39,36 +28,15 @@ def login():
 	user_email = input_data['email']
 	user_password = input_data['password']
 
-	temp = user_service.readByEmailandPassword(user_email,user_password)
+	return user_service.login_check(user_email,user_password)
 
-	
-	if(temp == "nothing" or temp == "False"):
-		return jsonify(
-			result = "Invalid Params!"
-			)
-	else:
-		input_role = temp[0]['role']
-		if(not input_role):
-			return "false"
 
-		additional_claims = {"role": f"{input_role}"}
-		return jsonify(result = "success",
-			# 검증된 경우, access 토큰 반환
-			access_token = create_access_token(identity = user_email,
-											additional_claims=additional_claims,
-											expires_delta = False)
-		)	
-	
-#################################################
-# 회원 전용 API 영역
 @auth.route("/protected", methods=["GET"])
 @jwt_required() #jwt check
 def protected():
     # Access the identity of the current user with get_jwt_identity
     current_user = get_jwt_identity()
     claims = get_jwt()
-    print(claims["role"])
-    print(current_user)
     return jsonify(logged_in_as=current_user), 200
 
 
@@ -77,8 +45,7 @@ def protected():
 @jwt_required()    # jwt check
 @admin_required()  # role : admin check
 def admin_test():
-    current_user = get_jwt_identity()
-    
+    current_user = get_jwt_identity()    
     return jsonify(logged_in_as=current_user), 200
 
 
@@ -87,6 +54,5 @@ def admin_test():
 @jwt_required()    # jwt check
 @user_required()   # role : user check
 def user_test():
-    current_user = get_jwt_identity()
-  
+    current_user = get_jwt_identity()  
     return jsonify(logged_in_as=current_user), 200
